07121be2abd78075e94ee6a57be4dac9fbfbbddb
trestletech <jeff.allen@trestletechnology.net>
2017-07-21 09:50:22 -0500

    Attempt PDF.

--- _build.sh
+++ _build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-Rscript -e "bookdown::render_book('index.Rmd', c('bookdown::gitbook', bookdown::epub_book')"
+Rscript -e "bookdown::render_book('index.Rmd', c('bookdown::gitbook', bookdown::epub_book', 'bookdown::pdf_book')"
