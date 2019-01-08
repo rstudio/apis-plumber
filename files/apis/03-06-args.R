413f50e9ee3da429373693d0a77f718c6c95fc5e
Layik Hama <layik.hama@gmail.com>
2019-01-08 17:05:04 +0000

    Addressing comments on PR

--- files/apis/03-06-args.R
+++ files/apis/03-06-args.R
@@ -1,7 +1,4 @@
 #' @post /submit
-#' get /submit
-#' add the above `get` tag for "get" to hit
-#' this function too.
 function(req){
   cat(req$args$email, req$args$bike, req$args$color)
 }
@@ -14,8 +11,8 @@ function(){
   <h2>Basic submit form</h2>
   <form action="http://localhost:8000/submit" method="post">
       <input type="text" placeholder="Email" name="email" required>
-      <input type="checkbox" name="bike" value="Bike"> I have a bike
-      <input type="text" placeholder="Bike colour" name="color">
+      <input type="checkbox" name="bike" value="bike"> I have a bike
+      <input type="text" placeholder="Bike color" name="color">
       <button type="submit">Submit</button>
   </form>
   '
